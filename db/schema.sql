66862ace80e4720cd150ac58e4b705ea09294014
Tucker Vassau <tuckv@hotmail.com>
2020-03-01 02:03:20 -0700

    Updated Schema

--- db/schema.sql
+++ db/schema.sql
@@ -7,5 +7,5 @@ USE burgers_db;
 CREATE TABLE burgers(
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     burger_name VARCHAR(80) NOT NULL,
-    devoured BOOLEAN DEFAULT false,
-);  
+    devoured BOOLEAN DEFAULT false
+);
