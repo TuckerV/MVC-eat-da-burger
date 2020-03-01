7b4938256bf99d6348cb6e75484d6cb6738f7467
Tucker Vassau <tuckv@hotmail.com>
2020-02-29 19:28:48 -0700

    ORM functions setup

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,44 @@
 const connection = require("../config/connection.js");
 
-let orm = {
-
+var orm = {
+    selectAll: function(cb){
+        let queryString = "SELECT * FROM burgers;";
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+          cb(result);
+        });
+    },
+    create: function(burger, cb) {
+        let queryString = "INSERT INTO burgers (burger_name) VALUES(?);";
+        connection.query(queryString, [burger], (err, res) => {
+            if (err){
+                throw err;  
+            } 
+            cb(res);
+        });
+    },
+    
+    update: function(id, cb) {
+        let queryString = "UPDATE burgers SET devoured = 1  WHERE id = ?;";
+        connection.query(queryString, [id], (err, res) => {
+            if (err){
+                throw err;  
+            } 
+            cb(res);  
+        });
+    },
+    delete: function(id, cb) {
+        let queryString = "DELETE FROM burgers WHERE id = ?";
+        connection.query(queryString, [id], (err, res) => {
+            if (err){
+                throw err;  
+            } 
+            cb(res);
+        });
+    }
 }
+
 
 module.exports = orm;
